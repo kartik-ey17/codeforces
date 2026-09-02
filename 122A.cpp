53929d148c2ce703915cdf30aa7eb7cd65e715c0
kartik-ey17 <kartikey.25bai11235@vitbhopal.ac.in>
2026-09-02 23:51:45 +0530

    122A

---
+++ 122A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> lucky = {4,7,44,47,74,77,444,447,474,477,744,747,774,777};
+    for(int l : lucky) {
+        if (n%l == 0) {
+            cout << "YES";
+            return 0;
+        }
+    }
+    cout << "NO";
+    return 0;
+}
